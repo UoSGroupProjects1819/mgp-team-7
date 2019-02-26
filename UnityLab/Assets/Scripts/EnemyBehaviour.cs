2383847c4632e7f80badd2905e9232198c0be4a7
JimmyVictor <rhukz@yahoo.com>
2019-02-26 18:39:26 +0000

    AI movement/shooting
    Implemented the ability for the AI to follow and run away, as well as shoot the player every two seconds.

---
+++ UnityLab/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+
+    private Transform player;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    public GameObject projectile;
+
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        timeBtwShots = startTimeBtwShots;
+	}
+	
+	
+	void Update ()
+    {
+        // if the enemy is too far away, he'll move closer to the target
+		if(Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+        // if he's close but not too close, the enemy will stop moving
+        else if(Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        // lastly, if the enemy is too close to the player, he'll retreat
+        else if(Vector2.Distance(transform.position, player.position) < retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+
+
+        if(timeBtwShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+	}
+}
