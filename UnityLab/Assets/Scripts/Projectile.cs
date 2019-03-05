dd81380907c4735c0b26e4a6e7e427393127a10d
JimmyVictor <rhukz@yahoo.com>
2019-03-05 14:50:44 +0000

    Shield Deflection
    the player is now able to deflect the bullet back

--- UnityLab/Assets/Scripts/Projectile.cs
+++ UnityLab/Assets/Scripts/Projectile.cs
@@ -10,34 +10,50 @@ public class Projectile : MonoBehaviour {
     private Transform wall;
     private Vector2 target;
 
+    private bool ceva = true;
+    public Rigidbody2D rb;
+    public float ReflectSpeed;
 
-	void Start ()
+    void Start ()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
 
         wall = GameObject.FindGameObjectWithTag("Wall").transform;
 
         target = new Vector2(player.position.x, player.position.y);
+        rb = GetComponent<Rigidbody2D>();
     }
 	
 
 	void Update ()
     {
         // the projectile moves towards the last known location of the player
+        if (ceva)
         transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
 
-        // if it reaches that location without hitting the player, destroy it
+        //if it reaches that location without hitting the player, destroy it
         if (transform.position.x == target.x && transform.position.y == target.y)
         {
             DestroyProjectile();
         }
-	}
+    }
 
     // if the projectile hits the player, destroy it
     void OnTriggerEnter2D(Collider2D other)
     {
+       // Debug.Log(other.name);
+        ceva = false;
         if (other.CompareTag("Player"))
             DestroyProjectile();
+
+        if ( other.CompareTag("Shield"))
+        {
+            // vector between the player and projectile
+            var dir = player.position - transform.position;
+            // reflect the projectile vector, update "up" to where the player is facing
+            var reflect = Vector2.Reflect(dir, player.up);
+            rb.velocity = reflect * ReflectSpeed;
+        }
     }
 
     // function that destroys the projectile
