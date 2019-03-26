38c78bdafa8e6089be4862be6ddbdd93f131584b
JimmyVictor <rhukz@yahoo.com>
2019-03-26 12:20:31 +0000

    Shield Recharge
    Implemented a function that gives the shield a recharge and an uptime time.

--- UnityLab/Assets/Scripts/PlayerShield.cs
+++ UnityLab/Assets/Scripts/PlayerShield.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerShield : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float cooldownTime;
+
+    public float shieldCharge;
+
+    private bool isReady = true;
+
+    public GameObject shield;
+
+    IEnumerator shieldControl()
     {
-        
+        shield.SetActive(true);
+        yield return new WaitForSecondsRealtime(shieldCharge);
+        shield.SetActive(false);
+        yield return new WaitForSecondsRealtime(cooldownTime);
+        isReady = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (isReady)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                isReady = false;
+                StartCoroutine(shieldControl());
+            }
+        }
         
     }
 }
